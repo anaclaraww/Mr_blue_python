0c2e713708b73ff2879ba6997e1110b3343a14fa
Ana Clara Melo <132848065+anaclaraw@users.noreply.github.com>
2024-06-07 23:57:58 -0300

    adicionado mais descrições

--- app.py
+++ app.py
@@ -2,8 +2,15 @@
 from ultralytics.utils.plotting import Annotator
 from ultralytics import YOLO
 
+def SimulandoCordenedadas():
+    lat = '-23.46636581'
+    long = '-51.84009933'
+    return lat, long
 
 def CriandoModel():
+    """
+    Função para criar e testar modelo
+    """
     #Criano=do modelo, com pré treinamento do YOLOv8, versão nano
     model = YOLO('yolov8n')
 
@@ -22,6 +29,7 @@ def CriandoModel():
     #Exibindo lista
     print("detections:",detections)
 
+    #Acessando boxes (caixas da detecção)
     for r in detections:
         print(r.boxes.data)
 
@@ -34,13 +42,16 @@ import os
 from PIL import Image
 
 def exibir_imagens(diretorio):
-    for arquivo in os.listdir(diretorio):
-        if arquivo.endswith((".jpg", ".jpeg", ".png")):
+    """
+    Função para Exibir a imagem detectada direto de suas pastas
+    """
+    for arquivo in os.listdir(diretorio): #percorre o diretório 
+        if arquivo.endswith((".jpg", ".jpeg", ".png")): #Condiciona pela extenção
             caminho_imagem = os.path.join(diretorio, arquivo)
             try:
                 imagem = Image.open(caminho_imagem)
                 imagem.show()
             except Exception as e:
-                print(f"Erro ao abrir imagem: {e}")
+                print(f"Erro ao abrir imagem: {e}")#Tratamento de erro, caso necessário
 
 exibir_imagens("runs/detect/predict")
